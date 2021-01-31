a0977ea63542b8ac22fda3d358d63b89e5105a0f
Pragyan-creator <60147873+Pragyan-creator@users.noreply.github.com>
2021-01-31 19:26:27 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var translateButton = document.querySelector("#translate-button");
+
+var translateInput = document.querySelector("#translate-input");
+
+var translateOutput = document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/dothraki.json"
+
+translateButton.onclick=function(){
+    var input=translateInput.value
+    var finalUrl = `${url}?text=${input}`
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET',`${finalUrl}`)
+    xhr.send()
+
+    xhr.onload=function(){
+        const data=JSON.parse(xhr.responseText)
+        console.log(data)
+        translateOutput.innerText=data.contents.translated
+    }
+    xhr.onerror=function(){
+        alert('Some error occured')
+    }
+}
